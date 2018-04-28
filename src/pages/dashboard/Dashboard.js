376450d4de6c047ca0dd59214aabec2266843b96
MARK SIKORA <Mark.Sikora@LibertyMutual.com>
2018-04-28 11:51:10 -0400

    Update text on dashboard main page to match ppt

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -12,35 +12,38 @@ class Dashboard extends React.Component {
         <h1 className="page-title">Agent <strong>Opportunity</strong></h1>
         <Row>
           <Col lg={6}>
-            <Widget title={<h5><span className="fw-semi-bold">Welcome</span></h5>}>
+            <Widget title={<h3><span className="fw-semi-bold">Welcome</span></h3>}>
               <div>
                 <p className="lead">
-                  You are looking at the spot where the Liberty Mutual
-                  Agent Opportunity is going to very soon.
+                  The Liberty Mutual Agent Opportunity provides insight into <strong>Split Rated Business Owners Policy</strong> (SRB).
+                  <br/><br/>
                   Built with <strong>React JS</strong> using
-                  Redux, React Router and Server Side Rendering!
+                  Redux, React Router and Server Side Rendering with Neo4j Graph Database!
                 </p>
                 <p className="fs-mini text-right m-0">Made for the Agents of <a href="https://www.libertymutual.com" target="_blank" rel="noopener noreferrer">Liberty Mutual</a>.</p>
               </div>
             </Widget>
-            <Widget title={<h5>Problem <span className="fw-semi-bold">Statement</span></h5>}>
+            <Widget title={<h3>Problem <span className="fw-semi-bold">Statement</span></h3>}>
               <div>
                 <p className="lead">
-                  This is where we state the problem.
+                  Competitive Dashboard or Prospecting Tools for Agents
+                   <br/><br/> 
+                   Give agents a view into how they are performing at selling Split Rated Business Owner Policy (SRB) policies vs their peers, 
+                   and give the business insight into performance and the classes of insurance being sold in the market.
                 </p>
               </div>
             </Widget>
-            <Widget title={<h5>Solution <span className="fw-semi-bold">Thesis</span></h5>}>
+            <Widget title={<h3>Solution <span className="fw-semi-bold">Thesis</span></h3>}>
               <div>
                 <p className="lead">
-                  A statement of our hypothesis. Short abstract of proposed solution.
+                Build a view into Policy SRB data that is agent focused and provide them with a rich and elegant experience.
                 </p>
               </div>
             </Widget>
-            <Widget title={<h5>Technical <span className="fw-semi-bold">Solution</span></h5>}>
+            <Widget title={<h3>Technical <span className="fw-semi-bold">Solution</span></h3>}>
               <div>
                 <p className="lead">
-                  A statement on how we solved it
+                Create an React UI application that has charts and graphs to visually represent Agent data, using actual datasets (Development) served from a graph database housing ACORD XML.
                 </p>
 
                 <p className="fs-mini lead">
